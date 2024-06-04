2e6598837de2ef726dc9b74ef1ff29a75686c917
BeshoiBotros <beshoibotros111@gmail.com>
2024-06-04 22:06:44 +0300

    .

--- cases/views.py
+++ cases/views.py
@@ -93,7 +93,7 @@ class SubCategoryView(APIView):
 
 
 class NewView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
         
